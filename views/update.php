6785f18d1bbc53ec6083e67a5d294e7521c04ffc
project79 <dejan79@gmail.com>
2010-12-06 11:10:05 +0100

    Info added

--- views/update.php
+++ views/update.php
@@ -1,6 +1,22 @@
 <?php
-/* 
- * Show and update note
+
+/**
+ * Notes plugin for Wolf CMS <http://project79.net/projects/notes>
+ * Available on Github <https://github.com/project79>
+ *
+ * Simple notes for admin area.
+ *
+ * @author Dejan Andjelkovic <dejan79@gmail.com>
+ * @package Wolf
+ * @subpackage plugin.notes
+ * @version 0.0.1
+ * @licence http://www.gnu.org/licenses/gpl.html
+ * @copyright http://project79.net ,2010
+ */
+
+/* Uncomment this for usage with Wolf 0.7.0 and/or above
+ * Security measure - from Wolf 0.7.0 above
+ * if (!defined('IN_CMS')) { exit(); }
  */
 
 ?>
