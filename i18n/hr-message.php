2536827422c9201df30c575ffb783f06dfb9c9bc
project79 <dejan79@gmail.com>
2010-12-09 14:18:19 +0100

    Croatian translation added

--- i18n/hr-message.php
+++ i18n/hr-message.php
@@ -1,7 +1,42 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-?>
+    /**
+     * Croatian file for plugin notes
+     *
+     * @package wolf
+     * @subpackage plugin.notes.translations
+     *
+     * @author Dejan Andjelkovic <dejan79@gmail.com>
+     * @version Wolf 0.6.0b
+     */
+
+    return array(
+    	'All notes' => 'Sve bilješke',
+	'All went well.' => 'Sve je prošlo ok',
+	'Cannot be changed.' => 'Nepromijenjivo',
+	'Content' => 'Sadržaj',
+	'Could not save this note!' => 'Nije moguće spremiti bilješku!',
+	'Could not update this note!' => 'Nije moguće ažurirati bilješku!',
+	'Create New Note' => 'Kreiraj novu bilješku',
+	'Created on' => 'Kreirano',
+	'Delete' => 'Obrisati',
+	'Documentation' => 'Dokumentacija',
+	'Edit' => 'Uredi',
+	'Edit Note' => 'Uredi bilješku',
+	'Edit content' => 'Uredi sadržaj',
+	'Edit title' => 'Uredi naslov',
+	'Have your notes always by your side.' => 'Imajte bilješke uvijek pri ruci',
+	'Insert title for your note.' => 'Unesite naslov za bilješku',
+	'Last updated on' => 'Zadnja izmjena',
+	'Main content for note.' => 'Glavni sadržaj za bilješku.',
+	'New Note' => 'Nova bilješka',
+	'Note ID' => 'ID bilješke',
+	'Notes' => 'Bilješke',
+	'Notes is disabled!' => 'Bilješke su onemogućene!',
+	'Plugin is succesfully uninstalled!' => 'Plugin je uspješno deinstaliran!',
+	'Save' => 'Spremi',
+	'Title' => 'Naslov',
+	'Unable to drop table :tablename' => 'Nije moguće obrisati tablicu :tablename',
+	'View All Notes' => 'Vidi sve bilješke',
+    );
+
