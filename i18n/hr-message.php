df5d329bb547b5db3fa526279fbb02d5ce3b963d
project79 <dejan79@gmail.com>
2010-12-09 12:41:28 +0100

    Croatian translation added

---
+++ i18n/hr-message.php
@@ -0,0 +1,41 @@
+<?php
+
+    /**
+     * Croatian file for plugin notes
+     *
+     * @package wolf
+     * @subpackage plugin.notes.translations
+     *
+     * @author Dejan Andjelkovic <dejan79@gmail.com>
+     * @version Wolf 0.6.0
+     */
+
+    return array(
+    	'All notes' => 'Sve bilješke',
+	'All went well.' => 'Sve je prošlo ok',
+	'Content' => 'Sadržaj',
+	'Could not save this note!' => 'Nije moguće spremiti ovu bilješku!',
+	'Could not update this note!' => 'Nije moguće ažurirati ovu bilješku!',
+	'Create New Note' => 'Kreiraj novu bilješku',
+	'Created on' => 'Kreirano',
+	'Delete' => 'Obrisati',
+	'Documentation' => 'Dokumentacija',
+	'Edit' => 'Uredi',
+	'Edit content.' => 'Uredi sadržaj',
+	'Edit title.' => 'Uredi naslov',
+	'Have your notes always by your side.' => 'Imajte bilješke uvijek pri ruci.',
+	'Insert title for your note.' => 'Upišite naslov za Vašu bilješku.',
+	'Last updated on' => 'Zadnja promjena',
+	'Main content for note.' => 'Sadržaj bilješke.',
+	'New Note' => 'Nova bilješka',
+	'Note ID' => 'ID bilješke',
+	'Notes' => 'Bilješke',
+	'Notes is disabled!' => 'Bilješke su onemogućene!',
+	'Preview only.' => 'Samo za pregled.',
+	'Save' => 'Spremi',
+	'Title' => 'NAslov',
+	'Unable to delete plugin settings.' => 'Nije moguće obrisati postavke plugina.',
+	'Unable to drop table :tablename' => 'Nije moguće obrisati tablicu :tablename',
+	'View All Notes' => 'Vidi sve bilješke',
+	'You have succesfully uninstalled Notes plugin!' => 'Uspješno ste deinstalirali plugin Bilješke!',
+    );
