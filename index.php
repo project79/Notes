e868085445d363de233fd4114054f5da5e8255eb
project79 <dejan79@gmail.com>
2010-12-09 13:24:50 +0100

    Extras deleted

--- index.php
+++ index.php
@@ -37,10 +37,3 @@ Plugin::addController('notes', __('Notes'), true);
 
 // Load Notes model
 AutoLoader::addFile('Notes', CORE_ROOT.'/plugins/notes/models/Notes.php');
-
-// Setup routes
-Dispatcher::addRoute(array(
-    '/notes/shownote/:num'  =>  '/plugin/notes/shownote/$1',
-    '/notes/update/:num'    =>  '/plugin/notes/update/$1',
-    '/notes/:num/delete'    =>  '/plugin/notes/delete/$1'
-));
