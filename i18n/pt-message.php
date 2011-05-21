7f3053422f067e0c71412f3ec7de3b7a1bf011bc
project79 <dejan79@gmail.com>
2011-05-21 21:46:25 +0200

    Portuguese translation added

---
+++ i18n/pt-message.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Portuguese file for plugin notes
+ *
+ * @package wolf
+ * @subpackage plugin.notes.translations
+ *
+ * @author Daniel Affonso <daniel.affonso@gmail.com>
+ * @version Wolf 0.7.5
+ */
+
+return array(
+    'All notes'                            => 'Todas as Notas',
+    'All went well.'                       => 'Tudo foi bem',
+    'Cannot be changed.'                   => 'Nao pode ser alterado',
+    'Content'                              => 'Conte&uacute;do',
+    'Could not save this note!'            => 'N&atilde;o foi poss&iacute;vel salvar essa nota',
+    'Could not update this note!'          => 'N&atilde;o foi poss&iacute;vel atualizar essa nota',
+    'Create New Note'                      => 'Criar nova nota',
+    'Created on'                           => 'Criado em',
+    'Delete'                               => 'Excluir',
+    'Documentation'                        => 'Documenta&ccedil;&atilde;o',
+    'Edit'                                 => 'Editar',
+    'Edit Note'                            => 'Editar nota',
+    'Edit content'                         => 'Editar conte&uacute;do',
+    'Edit title'                           => 'Editar t&iacute;tulo',
+    'Have your notes always by your side.' => 'Tenha suas notas sempre em m&atilde;os',
+    'Insert title for your note.'          => 'Insira o t&iacute;tulo para a sua nota',
+    'Last updated on'                      => 'Atualizado em',
+    'Main content for note.'               => 'Conte&uacute;da nota',
+    'New Note'                             => 'Nova Nota',
+    'Note ID'                              => 'ID da Nota',
+    'Notes'                                => 'Notas',
+    'Notes is disabled!'                   => 'O Plugin est&aacute; desabilitado',
+    'Plugin is succesfully uninstalled!'   => 'O Plugin foi desinstalado',
+    'Save'                                 => 'Salvar',
+    'Title'                                => 'T&iacute;tulo',
+    'Unable to drop table :tablename'      => 'Nao &eacute; poss&iacute;vel eliminar tabela :tablename',
+    'View All Notes'                       => 'Ver todas as notas',
+);
