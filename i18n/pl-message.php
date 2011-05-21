5b3c9ad9a58b0d736887bbc0b2c12a2fb2c5d973
project79 <dejan79@gmail.com>
2011-05-21 23:28:43 +0200

    Polish translation added

---
+++ i18n/pl-message.php
@@ -0,0 +1,42 @@
+<?php
+
+    /**
+     * Polish file for plugin notes
+     *
+     * @package wolf
+     * @subpackage plugin.notes.translations
+     *
+     * @author Michał Uchnast <djgprv@gmail.com> http://kreacjawww.pl/
+     * @version Wolf 0.7.5
+     *
+     */
+
+    return array(
+    'All notes' => 'Wszystkie notatki',
+    'All went well.' => 'Notatka została zapisana',
+    'Cannot be changed.' => 'Nie można dokonać zmian',
+    'Content' => 'Treść',
+    'Could not save this note!' => 'Nie udało się notatki!',
+    'Could not update this note!' => 'Nie udało się zmienić treści notatki!',
+    'Create New Note' => 'Twórz nową notatkę',
+    'Created on' => 'Czas utworzenia',
+    'Delete' => 'Usuń',
+    'Documentation' => 'Dokumentacja',
+    'Edit' => 'Edytuj',
+    'Edit Note' => 'Edytuj notatkę',
+    'Edit content' => 'Edytuj treść',
+    'Edit title' => 'Edytuj tytuł',
+    'Have your notes always by your side.' => 'Notatki zawsze pod ręką.',
+    'Insert title for your note.' => 'Wprowadź tytuł Twojej notatki.',
+    'Last updated on' => 'Ostatnia aktualizacja',
+    'Main content for note.' => 'Treść notatki',
+    'New Note' => 'Nowa notatka',
+    'Note ID' => 'ID notatki',
+    'Notes' => 'Notatki',
+    'Notes is disabled!' => 'Notatka jest wyłączona!',
+    'Plugin is succesfully uninstalled!' => 'Plugin został pomyślnie odinstalowany!',
+    'Save' => 'Zapisz',
+    'Title' => 'Tytuł',
+    'Unable to drop table :tablename' => 'Nie udało się usunąć tabeli :tablename',
+    'View All Notes' => 'Pokaż wszystkie notatki',
+    );
