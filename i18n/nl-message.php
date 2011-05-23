87f752ca274e82b33113247e88c23a5d75654451
project79 <dejan79@gmail.com>
2011-05-23 22:17:23 +0200

    dutch translation added

---
+++ i18n/nl-message.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Dutch file for plugin notes
+ *
+ * @package wolf
+ * @subpackage plugin.notes.translations
+ *
+ * @author Fortron <webmasterkubes@gmail.com>
+ * @version Wolf 0.7.5
+ */
+
+return array(
+    'All notes'                            => 'Alle notities',
+    'All went well.'                       => 'Notitie is bewaard',
+    'Cannot be changed.'                   => 'Kan niet gewijzigd worden.',
+    'Content'                              => 'Inhoud',
+    'Could not save this note!'            => 'Kon de notitie niet bewaren!',
+    'Could not update this note!'          => 'Kon de notitie niet bijwerken!',
+    'Create New Note'                      => 'Nieuwe notitie',
+    'Created on'                           => 'Aanmaak datum',
+    'Delete'                               => 'Verwijder',
+    'Documentation'                        => 'Documentatie',
+    'Edit'                                 => 'Bewerken',
+    'Edit Note'                            => 'Bewerk notitie',
+    'Edit content'                         => 'Inhoud van de notitie',
+    'Edit title'                           => 'Titel van de notitie',
+    'Have your notes always by your side.' => 'Heb je notities altijd naast je.',
+    'Insert title for your note.'          => 'Titel van de notitie',
+    'Last updated on'                      => 'Laatst bijgewerkt op',
+    'Main content for note.'               => 'Inhoud van de notitie',
+    'New Note'                             => 'Nieuwe notitie maken',
+    'Note ID'                              => 'Notitie ID',
+    'Notes'                                => 'Notities',
+    'Notes is disabled!'                   => 'Plugin notities is uitgeschakeld!',
+    'Plugin is succesfully uninstalled!'   => 'Plugin is succesvol gede&iuml;nstalleerd!',
+    'Save'                                 => 'Bewaren',
+    'Title'                                => 'Titel',
+    'Unable to drop table :tablename'      => 'Kan tabel :tablename niet verwijderen',
+    'View All Notes'                       => 'Alle notities weergeven',
+);
