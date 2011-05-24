38301fffd4a99e23263ff7b75ac338078c40cd55
project79 <dejan79@gmail.com>
2011-05-24 23:42:12 +0200

    Version update plus new functions

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  * @author Dejan Andjelkovic <dejan79@gmail.com>
  * @package Wolf
  * @subpackage plugin.notes
- * @version 0.0.7
+ * @version 0.0.8
  * @licence http://www.gnu.org/licenses/gpl.html
  * @copyright http://project79.net ,2010-2011
  */
@@ -22,7 +22,7 @@ Plugin::setInfos(array(
     'id'          => 'notes',
     'title'       => __('Notes'),
     'description' => __('Have your notes always by your side.'),
-    'version'     => '0.0.7',
+    'version'     => '0.0.8',
     'license'     => 'GPL',
     'author'      => 'Dejan Andjelkovic',
     'website'     => 'http://www.project79.net/',
@@ -35,3 +35,27 @@ Plugin::addController('notes', __('Notes'), 'admin_view', true);
 
 // Load Notes model
 AutoLoader::addFile('Notes', CORE_ROOT.'/plugins/notes/models/Notes.php');
+
+/* Show all notes in frontend
+ * Call it anywhere in the Page or Layout like this <?php showallnotes(); ?>
+ */
+function showallnotes(){
+    $notes = Notes::findAllFrom('Notes', 'id=id ORDER BY created_on DESC');
+    echo '<div id="show-all-notes">';
+        foreach ($notes as $n) {
+            echo '<h3>'.$n->getTitle().'</h3>';
+            echo $n->getContent();
+        }
+    echo '</div>';
+}
+
+/* Show note by $id
+ * Call it anywhere like this  <?php shownotebyid('ID_OF_NOTE'); ?>
+ */
+function shownotebyid($id) {
+    $note = Notes::findByIdFrom('Notes', $id);
+    echo '<div id="show-note-by-id">';
+    echo '<h3>'.$note->getTitle().'</h3>';
+    echo $note->getContent();
+    echo '</div>';
+}
