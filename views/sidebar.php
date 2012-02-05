135379e01c3a130e5238691d3cd64da7d15b403c
project79 <dejan79@gmail.com>
2012-02-05 23:32:08 +0100

    box for frontend display

--- views/sidebar.php
+++ views/sidebar.php
@@ -9,9 +9,9 @@
  * @author Dejan Andjelkovic <dejan79@gmail.com>
  * @package Wolf
  * @subpackage plugin.notes
- * @version 0.0.2
+ * @version 0.0.9
  * @licence http://www.gnu.org/licenses/gpl.html
- * @copyright http://project79.net ,2010
+ * @copyright http://project79.net, 2010-2012
  */
 
 
@@ -23,5 +23,14 @@ if (!defined('IN_CMS')) { exit(); }
 <p class="button"><a href="<?php echo get_url('plugin/notes/createnewnote'); ?>"><img src="<?php echo URI_PUBLIC; ?>wolf/plugins/notes/images/newnote.png" align="middle" alt="New Note" /> <?php echo __('New Note'); ?></a></p>
 <p class="button"><a href="<?php echo get_url('plugin/notes/tasks'); ?>"><img src="<?php echo URI_PUBLIC; ?>wolf/plugins/notes/images/view.png" align="middle" alt="View all notes" /> <?php echo __('View All Notes'); ?></a></p>
 <p class="button"><a href="<?php echo get_url('plugin/notes/documentation'); ?>"><img src="<?php echo URI_PUBLIC; ?>wolf/plugins/notes/images/docs.png" align="middle" alt="Docs" /> <?php echo __('Documentation'); ?></a></p>
+</div>
+
+<div class="box">
+    <h3>Show note in frontend</h3>
+    <p>There are two ways to display your notes in your layout, page or snippet.</p>
+    <h5>Show only one note</h5>
+    <p><code>&lt;?php shownotebyid('note_id'); ?&gt;</code></p>
+    <h5>Show all your notes</h5>
+    <p><code>&lt;?php showallnotes(); ?&gt;</code></p>
 </div>
 
