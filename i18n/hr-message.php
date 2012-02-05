71dc038021bbbaff1a9ec8613f72345fbfb793f6
project79 <dejan79@gmail.com>
2012-02-05 23:27:57 +0100

    Updated for 0.0.9

--- i18n/hr-message.php
+++ i18n/hr-message.php
@@ -3,18 +3,17 @@
     /**
      * Croatian file for plugin notes
      *
-     * @package wolf
-     * @subpackage plugin.notes.translations
+     * @package Plugins
+     * @subpackage notes
      *
      * @author Dejan Andjelkovic <dejan79@gmail.com>
      * @version Wolf 0.7.5
      */
 
     return array(
-	'All notes' => 'Sve bilješke',
+    	'All notes' => 'Sve bilješke',
 	'All went well.' => 'Sve je prošlo ok',
 	'Cancel' => 'Odustani',
-	'Cannot be changed.' => 'Nepromijenjivo',
 	'Content' => 'Sadržaj',
 	'Could not save this note!' => 'Nije moguće spremiti bilješku!',
 	'Could not update this note!' => 'Nije moguće ažurirati bilješku!',
@@ -24,12 +23,9 @@
 	'Documentation' => 'Dokumentacija',
 	'Edit' => 'Uredi',
 	'Edit Note' => 'Uredi bilješku',
-	'Edit content' => 'Uredi sadržaj',
-	'Edit title' => 'Uredi naslov',
 	'Have your notes always by your side.' => 'Imajte bilješke uvijek pri ruci',
-	'Insert title for your note.' => 'Unesite naslov za bilješku',
+	'Id' => 'Id',
 	'Last updated on' => 'Zadnja izmjena',
-	'Main content for note.' => 'Glavni sadržaj za bilješku.',
 	'New Note' => 'Nova bilješka',
 	'Note ID' => 'ID bilješke',
 	'Notes' => 'Bilješke',
@@ -39,5 +35,6 @@
 	'Title' => 'Naslov',
 	'Unable to drop table :tablename' => 'Nije moguće obrisati tablicu :tablename',
 	'View All Notes' => 'Vidi sve bilješke',
+	'Your note was successfully deleted' => 'Vaša bilješka je uspješno obrisana',
+	'none' => 'ništa',
     );
-
