ceb915e2ba63159a198761cd35f6eeb9c19d0bea
project79 <dejan79@gmail.com>
2012-02-05 23:26:47 +0100

    minor corrections 0.0.9
    deleted notice, filter_id and reorder of notes changed from created_on
    to id

--- NotesController.php
+++ NotesController.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- * Notes plugin for Wolf CMS <http://project79.net/projects/notes>
- * Available on Github <https://github.com/project79>
- *
- * Simple notes for admin area.
- *
- * @author Dejan Andjelkovic <dejan79@gmail.com>
- * @package Wolf
- * @subpackage plugin.notes
- * @version 0.0.2
- * @licence http://www.gnu.org/licenses/gpl.html
- * @copyright http://project79.net ,2010
- */
+* Notes plugin for Wolf CMS <http://project79.net/projects/notes>
+* Available on Github <https://github.com/project79>
+*
+* Simple notes for admin area.
+*
+* @author Dejan Andjelkovic <dejan79@gmail.com>
+* @package Wolf
+* @subpackage plugin.notes
+* @version 0.0.9
+* @licence http://www.gnu.org/licenses/gpl.html
+* @copyright http://project79.net, 2010-2012
+*/
 
 
 if (!defined('IN_CMS')) { exit(); }
@@ -46,7 +46,7 @@ class NotesController extends PluginController {
     public function documentation() {
         $this->display(NOTES_VIEWS_BASE.'/documentation');
     }
-	
+
     // Add new note
     public function createnewnote(){
         $this->display(NOTES_VIEWS_BASE.'/newnote');
@@ -54,7 +54,7 @@ class NotesController extends PluginController {
     
     // List all notes
     public function tasks() {
-        $notes = Notes::findAllFrom('Notes','id=id ORDER BY created_on DESC');
+        $notes = Notes::findAllFrom('Notes','id=id ORDER BY id DESC');
         $this->display(NOTES_VIEWS_BASE.'/tasks', array('notes' => $notes));
     }
     
@@ -72,26 +72,32 @@ class NotesController extends PluginController {
     public function delete($id) {
         $notes = Notes::findByIdFrom('Notes', $id);
         $notes->delete();
+        Flash::set('success', __('Your note was successfully deleted'));
 
         redirect(get_url('plugin/notes/tasks'));
     }
 
-    /*
-     * Create new note
-     */
+/*
+* Create new note
+*/
+
     public function newnote(){
+        //
 
             if (!isset($_POST['save'])) {
                 Flash::set('error', __('Could not save this note!'));
             }
             else {
                 $data = $_POST['notes'];
-
+                
+				
                 $notes = new Notes();
-
+                
                 $notes->title = $data['title'];
+                $notes->filter_id = $data['filter_id'];
                 $notes->content = $data['content'];
-                $notes->created_on = date('Y-m-d'); // da vidimo hoce li raditi
+                $notes->created_on = date('Y-m-d');
+                
                 $notes->save();
 
                 Flash::set('success', __('All went well.'));
@@ -100,9 +106,9 @@ class NotesController extends PluginController {
         
     }
 
-    /*
-     * Update note
-     */
+/*
+* Update note
+*/
     public function updatenote(){
 
             if (!isset($_POST['save'])) {
@@ -111,14 +117,16 @@ class NotesController extends PluginController {
             else {
                 $data = $_POST['notes'];
                 use_helper('Kses');
-                $data['id'] = kses(trim($data['id']), array()); 
+                $data['id'] = kses(trim($data['id']), array());
 
                 $notes = new Notes();
 
                 $notes->id = $data['id'];
                 $notes->title = $data['title'];
+                $notes->filter_id = $data['filter_id'];
                 $notes->content = $data['content'];
                 $notes->updated_on = date('Y-m-d');
+                
                 $notes->save();
 
                 Flash::set('success', __('All went well.'));
