9abc28692a7e1f77cad80b0ab0149e74c9eb0929
project79 <dejan79@gmail.com>
2012-02-05 23:33:39 +0100

    added id, default icons and hover effect

--- views/tasks.php
+++ views/tasks.php
@@ -9,9 +9,9 @@
  * @author Dejan Andjelkovic <dejan79@gmail.com>
  * @package Wolf
  * @subpackage plugin.notes
- * @version 0.0.8
+ * @version 0.0.9
  * @licence http://www.gnu.org/licenses/gpl.html
- * @copyright http://project79.net ,2010-2011
+ * @copyright http://project79.net, 2010-2012
  */
 
 if (!defined('IN_CMS')) { exit(); }
@@ -20,27 +20,29 @@ if (!defined('IN_CMS')) { exit(); }
 ?>
 
 <h1><?php echo __('All notes'); ?></h1>
-<div id="notes">
-    <table class="fieldset" cellspacing="0" cellpadding="0" border="0">
-        <tr>
-			<td class="naslov"><?php echo __('Id'); ?></td>
-            <td class="naslov"><?php echo __('Title'); ?></td>
-            <td class="naslov"><?php echo __('Created on'); ?></td>
-            <td class="naslov"><?php echo __('Last updated on'); ?></td>
-            <td class="naslov" align="right"><?php echo __('Edit'); ?></td>
-            <td class="naslov"><?php echo __('Delete'); ?></td>
-        </tr>
+    <table class="index" cellspacing="0" cellpadding="0" border="0">
+        <thead>
+            <tr>
+                <td><?php echo __('Id'); ?></td>
+                <td><?php echo __('Title'); ?></td>
+                <td><?php echo __('Created on'); ?></td>
+                <td><?php echo __('Last updated on'); ?></td>
+                <td><?php echo __('Edit'); ?></td>
+                <td><?php echo __('Delete'); ?></td>
+            </tr>
+        </thead>
         
         <?php foreach($notes as $note) { ?>
 
-        <tr>
-			<td><?php echo $note->getId(); ?></td>
+        <tr class="<?php echo odd_even(); ?>">
+            <td><?php echo $note->getId(); ?></td>
             <td><a href="<?php echo get_url('plugin/notes/shownote/'.$note->id); ?>"><?php echo $note->getTitle(); ?></a></td>
             <td><?php echo $note->getDate(); ?></td>
             <td><?php echo $note->getUpdate(); ?></td>
-            <td align="right"><a href="<?php echo get_url('plugin/notes/update/'.$note->id); ?>"><img src="<?php echo URI_PUBLIC; ?>wolf/plugins/notes/images/edit.gif" align="middle" title="<?php echo __('Edit'); ?>" alt="<?php echo __('Edit'); ?>" /></a></td>
-            <td><a href="<?php echo get_url('plugin/notes/delete/'.$note->id); ?>"><img src="<?php echo URI_PUBLIC; ?>wolf/plugins/notes/images/trash.gif" align="middle" title="<?php echo __('Delete'); ?>" alt="<?php echo __('Delete'); ?>" /></a></td>
+            <td align="right"><a href="<?php echo get_url('plugin/notes/update/'.$note->id); ?>"><img src="<?php echo URI_PUBLIC; ?>wolf/icons/rename-16.png" alt="Edit" /></a></td>
+            <td><a href="<?php echo get_url('plugin/notes/delete/'.$note->id); ?>"><img src="<?php echo URI_PUBLIC; ?>wolf/icons/delete-16.png" alt="Delete" /></a></td>
         </tr>
         <?php } ?>
     </table>
-</div>
+
+ 
