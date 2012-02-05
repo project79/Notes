6e67979e9bc9d4cc758427e1edb1d2f4228f5b87
project79 <dejan79@gmail.com>
2012-02-05 23:29:52 +0100

    extra columns added
    filter_id, content_html and before_save

--- models/Notes.php
+++ models/Notes.php
@@ -9,9 +9,9 @@
  * @author Dejan Andjelkovic <dejan79@gmail.com>
  * @package Wolf
  * @subpackage plugin.notes
- * @version 0.0.1
+ * @version 0.0.9
  * @licence http://www.gnu.org/licenses/gpl.html
- * @copyright http://project79.net ,2010
+ * @copyright http://project79.net, 2010-2012
  */
 
 if (!defined('IN_CMS')) { exit(); }
@@ -21,9 +21,20 @@ class Notes extends Record {
     const TABLE_NAME = 'notes';
 
     public $id;
+    public $filter_id;
+    public $title;
+    public $content;
+    public $content_html;
+    public $created_on;
+    public $updated_on;
+
 
     public function getId() {
         return $this->id;
+    }
+	
+    public function getFilter() {
+	return $this->filter_id;
     }
 
     public function getTitle(){
@@ -33,6 +44,20 @@ class Notes extends Record {
     public function getContent(){
 	return $this->content;
     }
+    
+    public function frontendContent(){
+        return $this->content_html;
+    }
+
+
+    public function showContent(){
+        if(($this->filter_id) != 0) {
+            return $this->content;
+        }
+        else {
+            return $this->content_html;
+        }
+    }
 
     public function getDate(){
         return $this->created_on;
@@ -40,6 +65,17 @@ class Notes extends Record {
 
     public function getUpdate(){
         return $this->updated_on;
+    }
+    
+    public function beforeSave() {
+    // stolen from snippet :)    
+    if ( ! empty($this->filter_id)) {
+        $this->content_html = Filter::get($this->filter_id)->apply($this->content);
+    }
+    else {
+        $this->content_html = $this->content;
+    }
+    return true;
     }
   
 }
